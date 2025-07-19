bfad8ef9e5192fafffae31757f5f56dd2362b6f0
Popie52 <popiesailor@gmail.com>
2025-07-19 12:15:49 +0530

    fix comments

--- part8/8.8-8.12/library-frontend/src/App.jsx
+++ part8/8.8-8.12/library-frontend/src/App.jsx
@@ -1,10 +1,8 @@
-// import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import { Route, Routes, Link } from "react-router-dom";
 const App = () => {
-  // const [page, setPage] = useState("authors");
   const styleLinks = {
     padding: '10px',
     display: 'flex',
@@ -22,9 +20,6 @@ const App = () => {
         <Link style={linkStyle} to="/" >authors</Link>
         <Link to="/books" style={linkStyle}>books</Link>
         <Link to="/addBook" style={linkStyle}>add book</Link>
-        {/* <button onClick={() => setPage("authors")}>authors</button> */}
-        {/* <button onClick={() => setPage("books")}>books</button> */}
-        {/* <button onClick={() => setPage("add")}>add book</button> */}
       </div>
 
       <Routes>
@@ -32,12 +27,6 @@ const App = () => {
         <Route path="/books" element={<Books  />} /> 
         <Route path="/addBook" element={<NewBook  />} /> 
       </Routes>
-
-      {/* <Authors show={page === "authors"} /> */}
-
-      
-
-      {/* <NewBook show={page === "add"} /> */}
     </div>
   );
 };
